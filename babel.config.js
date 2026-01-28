4519b7b1ce09c7248363b735d24fa785c79d07a6
Reseanne Rampou <reseanner@gmail.com>
2026-01-28 21:51:09 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["@babel/plugin-syntax-flow"],
+    plugins: [
+      "@babel/plugin-syntax-flow",
+      "nativewind/babel",
+    ],
   };
 };
