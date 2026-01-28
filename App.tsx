c9237a05e838f10edd1a7f62bf5fc7918e25ca34
Reseanne Rampou <reseanner@gmail.com>
2026-01-28 22:18:03 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
-import { View, Text } from "react-native";
+import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-brown-500">
-        Coffee Shop ☕
-      </Text>
-    </View>
-  );
+  return <HomeScreen />;
 }
