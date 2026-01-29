bdd325c54888d6c7dd6078ecf975d7fb2d49ce94
sean-techie <reseanner@gmail.com>
2026-01-29 13:46:23 +0200

    Add coffee shop screens and navigation

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+<<<<<<< HEAD
     presets: ["babel-preset-expo"],
     plugins: ["@babel/plugin-syntax-flow"],
+=======
+    presets: ['babel-preset-expo'],
+>>>>>>> 9b7111c (Add coffee shop screens and navigation)
   };
 };
