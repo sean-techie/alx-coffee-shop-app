790f8a892c118d6cdd76edd187667edca0d53050
Reseanne Rampou <reseanner@gmail.com>
2026-01-29 09:42:50 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
-import HomeScreen from "./app/screens/HomeScreen";
+import { View, Text } from "react-native";
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Coffee Shop App ☕</Text>
+    </View>
+  );
 }
