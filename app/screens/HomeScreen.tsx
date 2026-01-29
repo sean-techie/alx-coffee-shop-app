484b59c2a56e0d807df861b8719080b49d6c4dd3
Reseanne Rampou <reseanner@gmail.com>
2026-01-29 09:42:20 +0200

    Update HomeScreen.tsx

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,14 +1,1 @@
-import { View, Text } from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-brown-800">
-        ☕ Coffee Shop Home
-      </Text>
-      <Text className="mt-2 text-gray-500">
-        NativeWind is working!
-      </Text>
-    </View>
-  );
-}
