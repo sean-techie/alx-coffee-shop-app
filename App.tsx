af3e8f8a301d501b4399825380eab4e9bc329c49
Reseanne Rampou <reseanner@gmail.com>
2026-01-29 09:54:55 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Coffee Shop App ☕</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Coffee Shop App</Text>
     </View>
   );
 }
